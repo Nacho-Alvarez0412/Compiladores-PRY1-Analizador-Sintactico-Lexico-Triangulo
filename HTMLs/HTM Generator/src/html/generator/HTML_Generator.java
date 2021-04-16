cdfe0e0b6fea792103677c546d96706013291ed4
Ignacio Alvarez <nacho.alvarez2001@gmail.com>
2021-04-16 14:41:16 -0600

    Structure Created

---
+++ HTMLs/HTM Generator/src/html/generator/HTML_Generator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package html.generator;
+
+/**
+ *
+ * @author nacho
+ */
+public class HTML_Generator {
+    
+}
---
+++ HTMLs/HTM Generator/src/html/generator/HTML_Tokens.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package html.generator;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.isLiteral;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author nacho
+ */
+public class HTML_Tokens {
+    private String spelling;
+    private HTML_Token_Enum type;
+    
+    private static String[] reservedWords = new String[] {"array","choose","const","do","else","elsif","end",
+                                                          "for","from","func","if","in","let","loop","nothing",
+                                                          "of","package","private","proc","record","recursive",
+                                                          "then","to","type","until","var","when","while" 
+                                                        };
+    private static String[] operators = new String[] {".",":",";",",",":=","~","|","$",".."};
+    private static String[] separators = new String[] {"(",")","[","]","{","}","",};
+    
+    public HTML_Tokens(String spelling){
+        this.spelling = spelling;
+        this.type = identifyType(spelling);
+    }
+
+    private HTML_Token_Enum identifyType(String spelling) {
+        if (spelling.contentEquals("\t")){
+            return HTML_Token_Enum.TAB;
+        }
+        else if (spelling.contentEquals(" ")){
+            return HTML_Token_Enum.SPACE;
+        }
+        
+        else if (isReserved(spelling)){
+            return HTML_Token_Enum.RESERVED;
+        }
+        
+        else if (isOperator(spelling)){
+            return HTML_Token_Enum.OPERATOR;
+        }
+        
+        else if (isSeparator(spelling)){
+            return HTML_Token_Enum.SEPARATOR;
+        }
+        
+        else if (isLiteral(spelling)){
+            return HTML_Token_Enum.LITERAL;
+        }
+        
+        else if (spelling.charAt(0) == '!'){
+            return HTML_Token_Enum.COMMENT;
+        }
+        return HTML_Token_Enum.IDENTIFIER;
+    }
+
+    private boolean isReserved(String spelling) {
+        for (String reservedWord : HTML_Tokens.reservedWords) {
+            if (spelling.contentEquals(reservedWord)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean isOperator(String spelling) {
+        for (String operator : HTML_Tokens.operators) {
+            if (spelling.contentEquals(operator)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean isSeparator(String spelling) {
+        for (String separator : HTML_Tokens.separators) {
+            if (spelling.contentEquals(separator)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private final Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false; 
+        }
+        return pattern.matcher(strNum).matches();
+    }
+    
+    public boolean isLiteral(String spelling){
+        return (isNumeric(spelling) || isChar(spelling));
+        
+    }
+    
+    public boolean isChar(String spelling){
+        return (spelling.charAt(0) == '\'') && (spelling.charAt(spelling.length()-1) == '\'');
+    }
+    
+    @Override
+    public String toString(){
+        return this.spelling;
+    }
+
+    public HTML_Token_Enum getType() {
+        return type;
+    }
+            
+}
